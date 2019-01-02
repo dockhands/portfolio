4563b8a89abf99d93f78dcfdcf170b9587977009
Dan Hawkins <danhawkins4@gmail.com>
2019-01-02 12:58:25 -0800

    update portfolio layout

---
+++ js/main.js
@@ -0,0 +1,153 @@
+
+
+
+
+'use strict';
+
+
+$(window).on('load', function() { 
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+	if($('.portfolios-area').length > 0 ) {
+		var containerEl = document.querySelector('.portfolios-area');
+		var mixer = mixitup(containerEl);
+	}
+
+});
+
+
+(function($) {
+
+	/*------------------
+		Navigation
+	--------------------*/
+	$('.nav-switch').on('click', function(event) {
+		$('.main-menu').slideToggle(400);
+		event.preventDefault();
+	});
+
+
+
+	/*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+
+	/*----------------------
+		Portfolio layout
+	------------------------*/
+	var port_fi =  $('.portfolios-area .first-item'),
+		port_si =  $('.portfolios-area .second-item'),
+		port_intro_h =  $('.portfolio-intro').innerHeight();
+
+	if ($(window).width() > 991) {
+		port_fi.appendTo('.portfolio-intro');
+		port_si.find('.portfolio-item').height(port_intro_h + 601);
+	}
+
+	$('.portfolio-item.pi-style2').each(function() {
+		var pi_width = $(this).width();
+		$(this).height(pi_width + 50);
+	});
+
+
+
+	/*------------------
+		Popup
+	--------------------*/
+	$('.portfolio-item-img').magnificPopup({
+		type: 'image',
+		mainClass: 'img-popup-warp',
+		removalDelay: 400,
+	});
+
+
+	/*------------------
+		Accordions
+	--------------------*/
+	$('.panel-link').on('click', function (e) {
+		$('.panel-link').parent('.panel-header').removeClass('active');
+		var $this = $(this).parent('.panel-header');
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+		e.preventDefault();
+	});
+
+
+if($().circleProgress){
+
+	//Set progress circle 1
+	$("#progress1").circleProgress({
+		value: 0.75,
+		size: 146,
+		thickness: 3,
+		fill: "#4286f4",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 2
+	$("#progress2").circleProgress({
+		value: 0.83,
+		size: 146,
+		thickness: 3,
+		fill: "#4286f4",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 3
+	$("#progress3").circleProgress({
+		value: 0.25,
+		size: 146,
+		thickness: 3,
+		fill: "#4286f4",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 4
+	$("#progress4").circleProgress({
+		value: 0.95,
+		size: 146,
+		thickness: 3,
+		fill: "#e55234",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+}
+
+})(jQuery);
+
+
+/*------------------
+		DEsCRIPTION
+--------------------*/
+var words = [
+	"writes code",
+	"writes copy",
+	"writes wrongs",
+	"does research",
+	"does watercolours",
+	"does favours",
+	"makes comics",
+	"makes apps",
+	"makes slow cooked meals",
+	"goes for walks",
+	"goes for bust",
+	"creates videos",
+	"creates mistaekes",
+	];
+	var count = 0;
+	function rotateWord() {
+	  var current_word = words[count]; 
+	  $("#rotate_word").html(current_word); 
+	  count++; 
+	  if (count == words.length) { count = 0; } 
+	  setTimeout(rotateWord, 2000); 
+	}
+	rotateWord();
+
